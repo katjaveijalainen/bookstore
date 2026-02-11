7d65ed34c864e3b88768b9854baf2e36e133ed14
katjaveijalainen <162602348+katjaveijalainen@users.noreply.github.com>
2026-02-11 19:16:15 +0200

    BookController.java

--- src/main/java/bookstore/web/BookController.java
+++ src/main/java/bookstore/web/BookController.java
@@ -1,13 +1,23 @@
 package bookstore.web;
 
+import bookstore.domain.Book;
+import bookstore.domain.BookRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class BookController {
 
-    @GetMapping("/index")
-    public String index() {
-        return "index";
+    private final BookRepository repository;
+
+    public BookController(BookRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/booklist")
+    public String bookList(Model model) {
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
     }
 }
