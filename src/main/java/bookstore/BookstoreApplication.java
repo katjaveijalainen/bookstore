cf3c84d1077cf70a7cadbed72a1feb5ba0f8d2e5
katjaveijalainen <162602348+katjaveijalainen@users.noreply.github.com>
2026-02-11 19:46:32 +0200

    BookstoreApplication.java

--- src/main/java/bookstore/BookstoreApplication.java
+++ src/main/java/bookstore/BookstoreApplication.java
@@ -1,24 +1,28 @@
 package bookstore;
 
+import bookstore.domain.Book;
+import bookstore.domain.BookRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import bookstore.domain.Book;
-import bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookstoreApplication.class, args);
-	}
-	@Bean
-public CommandLineRunner demo(BookRepository repo) {
-    return (args) -> {
-        repo.save(new Book("Humiseva Harju", "Emily Bronte", 2008, "12345", 25.60));
-        repo.save(new Book("Harry Potter ja Viisasten kivi", "J.K.Rowling", 2018, "67890", 15.70));
-    };
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(BookRepository repo) {
+        return (args) -> {
+            repo.save(new Book("Humiseva Harju", "Emily Bronte", 2008, "12345", 25.60));
+            repo.save(new Book("Harry Potter ja Viisasten kivi", "J.K.Rowling", 2018, "67890", 15.70));
+        };
+    }
 }
+
 
 
 
